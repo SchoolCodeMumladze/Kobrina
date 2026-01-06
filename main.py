2284d3dd48ef4c2aec01f2d541bda8d71ac18274
KobrinaM <237300369+MashaKobrina@users.noreply.github.com>
2026-01-06 17:38:41 +0300

    Добаления вычисления синуса,косинуса,тангенса,котангенса и изменение вывода истории действий

--- main.py
+++ main.py
@@ -49,8 +49,24 @@ while True:
         a=int(input())
         print(math.factorial(a))
         c.append(f"Факториал числа {a}={math.factorial(a)}")
+    if op=="Синус":
+        a=int(input())
+        print(math.sin(a))
+        c.append(f"Синус числа {a}={math.sin(a)}")
+    if op=="Косинус":
+        a=int(input())
+        print(math.cos(a))
+        c.append(f"Косинус числа {a}={math.cos(a)}")
+    if op=="Тангенс":
+        a=int(input())
+        print(math.tan(a))
+        c.append(f"Тангенс числа {a}={math.tan(a)}")
+    if op=="Котангенс":
+        a=int(input())
+        print(math.atan(a))
+        c.append(f"Котангенс числа {a}={math.atan(a)}")
     if op=="История" or op=="история":
-        print(f"Ваша история запросов:{c}")
+        print(f"Ваша история запросов:",*c,sep="\n")
         print("Хотите продолжить?")
         n=input()
         if n=="Нет" or n=="No" or n=="нет" or n=="no":
